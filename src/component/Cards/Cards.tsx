6348c593e013ad46eb9f42fb1683aed7e21b08ef
Samim Uzzaman Sunny <samimuzzamansunny@Samims-MacBook-Air.local>
2026-09-20 08:04:12 +0600

    last git push

--- src/component/Cards/Cards.tsx
+++ src/component/Cards/Cards.tsx
@@ -1,4 +1,4 @@
-import React, { use, useState } from 'react';
+import { use, useState } from 'react';
 import type { Icard } from '../../types/cardType';
 import Card from './Card';
 import SelectedCard from './SelectedCard';
